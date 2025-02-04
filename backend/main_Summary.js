20f905c23eaf4f51e47ed74052e690ae7659bdb3
Sagar <sp1879482@gmail.com>
2025-02-04 17:58:24 +0530

    Some Code Of Print Added

--- backend/main_Summary.js
+++ backend/main_Summary.js
@@ -13,6 +13,9 @@ document.addEventListener("DOMContentLoaded", () => {
   localStorage.removeItem("selectedSymptoms");
   localStorage.removeItem("selectedDiagnosis");
   localStorage.removeItem("selectedMedicalHistory");
+  localStorage.removeItem("selectedSurgicalHistory");
+  localStorage.removeItem("selectedInvestigation");
+  localStorage.removeItem("selectedAdvices");
 });
 
 // Toggle selection for symptoms, medical history, and surgical history
@@ -56,6 +59,29 @@ function toggleSymptoms(element) {
     }
   }
 
+
+  function toggleSurgicalHistory(element) {
+    try {
+      // Toggle the 'selected' class
+      element.classList.toggle("selected");
+  
+      // Get all items with the 'selected' class
+      const selectedItems = Array.from(
+        document.querySelectorAll(".surgicalHistory.selected")
+      ).map((item) => item.textContent.trim());
+  
+      // Save the selected items to local storage
+      localStorage.setItem("selectedSurgicalHistory", JSON.stringify(selectedItems));
+  
+      // Log the updated array for debugging
+      console.log(selectedItems);
+    } catch (e) {
+      console.error("An error occurred:", e);
+    }
+  }
+
+
+
   function toggleDiagnosis(element) {
     try {
       // Toggle the 'selected' class
@@ -75,8 +101,50 @@ function toggleSymptoms(element) {
       console.error("An error occurred:", e);
     }
   }
+
+  function toggleInvestigation(element) {
+    try {
+      // Toggle the 'selected' class
+      element.classList.toggle("selected");
+  
+      // Get all items with the 'selected' class
+      const selectedItems = Array.from(
+        document.querySelectorAll(".investigation.selected")
+      ).map((item) => item.textContent.trim());
+  
+      // Save the selected items to local storage
+      localStorage.setItem("selectedInvestigation", JSON.stringify(selectedItems));
+  
+      // Log the updated array for debugging
+      console.log(selectedItems);
+    } catch (e) {
+      console.error("An error occurred:", e);
+    }
+  }
+
+
+  function toggleAdvices(element) {
+    try {
+      // Toggle the 'selected' class
+      element.classList.toggle("selected");
+  
+      // Get all items with the 'selected' class
+      const selectedItems = Array.from(
+        document.querySelectorAll(".advice.selected")
+      ).map((item) => item.textContent.trim());
+  
+      // Save the selected items to local storage
+      localStorage.setItem("selectedAdvices", JSON.stringify(selectedItems));
+  
+      // Log the updated array for debugging
+      console.log(selectedItems);
+    } catch (e) {
+      console.error("An error occurred:", e);
+    }
+  }
   
 document.getElementById("print-btn").addEventListener("click", function () {
+  // Store Vision Table the dictionary name and value
   const visionTable = {
     UnaidedR : document.getElementById("UnaidedR").value,
     UnaidedL : document.getElementById("UnaidedL").value,
@@ -143,11 +211,107 @@ document.getElementById("print-btn").addEventListener("click", function () {
     JSON.stringify(currentPowerGlasses)
   );
 
+  // Store Eye Examination Table the dictionary name and value
+  const eyeExaminationTable = {
+    EyelidsR: document.getElementById("EyelidsR").value,
+    EyelidsL: document.getElementById("EyelidsL").value,
+    ConjunctivaR: document.getElementById("ConjunctivaR").value,
+    ConjunctivaL: document.getElementById("ConjunctivaL").value,
+    ScleraR: document.getElementById("ScleraR").value,
+    ScleraL: document.getElementById("ScleraL").value,
+    CorneaR: document.getElementById("CorneaR").value,
+    CorneaL: document.getElementById("CorneaL").value,
+    AnteriorR: document.getElementById("AnteriorR").value,
+    AnteriorL: document.getElementById("AnteriorL").value,
+    IrisR: document.getElementById("IrisR").value,
+    IrisL: document.getElementById("IrisL").value,
+    PupilR: document.getElementById("PupilR").value,
+    PupilL: document.getElementById("PupilL").value,
+    LensR: document.getElementById("LensR").value,
+    LensL: document.getElementById("LensL").value,
+    FundusR: document.getElementById("FundusR").value,
+    FundusL: document.getElementById("FundusL").value,
+  }
+
+  console.log(eyeExaminationTable);
+
+  // Save the values in local storage
+  localStorage.setItem(
+    "eyeExaminationTable",
+    JSON.stringify(eyeExaminationTable)
+  );
+
+  // Store Refraction Table the dictionary name and value
+  const refractionTable = {
+    DistanceSPH : document.getElementsByName("Distance-SPH")[0].value,
+    DistanceCYL : document.getElementsByName("Distance-CYL")[0].value,
+    DistanceAxis : document.getElementsByName("Distance-Axis")[0].value,
+    DistanceVA : document.getElementsByName("Distance-VA")[0].value,
+    DistanceSPH2 : document.getElementsByName("Distance-SPH2")[0].value,
+    DistanceCYL2 : document.getElementsByName("Distance-CYL2")[0].value,
+    DistanceAxis2 : document.getElementsByName("Distance-Axis2")[0].value,
+    DistanceVA2 : document.getElementsByName("Distance-VA2")[0].value,
+    NearSPH : document.getElementsByName("Near-SPH")[0].value,
+    NearCYL : document.getElementsByName("Near-CYL")[0].value,
+    NearAxis : document.getElementsByName("Near-Axis")[0].value,
+    NearVA : document.getElementsByName("Near-VA")[0].value,
+    NearSPH2 : document.getElementsByName("Near-SPH2")[0].value,
+    NearCYL2 : document.getElementsByName("Near-CYL2")[0].value,
+    NearAxis2 : document.getElementsByName("Near-Axis2")[0].value,
+    NearVA2 : document.getElementsByName("Near-VA2")[0].value,
+    REIPD : document.getElementById("RE-IPD").value,
+    LEIPD : document.getElementById("LE-IPD").value,
+    RETypeOfGlass : document.getElementById("RE-TypeOfGlass").value,
+    LETypeOfGlass : document.getElementById("LE-TypeOfGlass").value,
+    RERemark : document.getElementById("RE-Remark").value,
+    LERemark : document.getElementById("LE-Remark").value,
+  };
+
+  console.log(refractionTable);
+
+  // Save the values in local storage
+  localStorage.setItem(
+    "refractionTable",
+    JSON.stringify(refractionTable)
+  );
+
+
+   // Get the medicine entries
+   const medicineEntries = document.querySelectorAll(".medicine-entry");
+
+   // Store the medicine entries in an array
+   const medicines = [];
+
+   // Loop through each medicine entry
+
+   medicineEntries.forEach((entry, index) => {
+     const medicineSelect =
+       entry.querySelector(".medicine-select").value;
+     const medicineDose = entry.querySelector(".medicine-dose").value;
+     const medicineQuantity =
+       entry.querySelector(".medicine-quantity").value;
+
+     // Store the medicine entry in a dictionary
+     const medicine = {
+       medicineSelect: medicineSelect,
+       medicineDose: medicineDose,
+       medicineQuantity: medicineQuantity,
+     };
+
+     // Push the medicine entry to the medicines array
+     medicines.push(medicine);
+   });
+
+   console.log(medicines);
+
+    // Save the medicines array to local storage
+    localStorage.setItem("medicines", JSON.stringify(medicines));
+
   // Open print.html using href
-  window.location.href = "print.html";
+  // window.location.href = "print.html";
 
   // Open print.html using window.open
-  //  window.open("print.html", "_blank");
+   window.open("print.html", "_blank");
   
 });
  
