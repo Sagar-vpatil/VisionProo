ff99bc2e62684787957bdd8ce5019c36b153edff
Sagar <125073576+Sagar-vpatil@users.noreply.github.com>
2026-09-09 14:42:46 +0530

    Bill No Added Into Firestore Database

--- backend/firebaseConfig.js
+++ backend/firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 // Import the required Firebase modules
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
-import { getFirestore, collection, addDoc, getDocs, query, orderBy, limit, setDoc, doc, where, getDoc, updateDoc, deleteDoc,enableIndexedDbPersistence, startAfter, endBefore, limitToLast} from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js'
+import { getFirestore, collection, addDoc, getDocs, query, orderBy, limit, setDoc, doc, where, getDoc, updateDoc, deleteDoc,enableIndexedDbPersistence, startAfter, endBefore, limitToLast, runTransaction} from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js'
 import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,15 +9,15 @@ import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/fir
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD6RUMVTE0GpI6qEiHuI-uw4oDFIDDtItY",
-    authDomain: "eye-hospital-ee88e.firebaseapp.com",
-    projectId: "eye-hospital-ee88e",
-    storageBucket: "eye-hospital-ee88e.firebasestorage.app",
-    messagingSenderId: "173931536781",
-    appId: "1:173931536781:web:fbf50296e7b05d57235dc4",
-    measurementId: "G-GWM7MZ1FJC"
+  apiKey: "AIzaSyD6RUMVTE0GpI6qEiHuI-uw4oDFIDDtItY",
+  authDomain: "eye-hospital-ee88e.firebaseapp.com",
+  projectId: "eye-hospital-ee88e",
+  storageBucket: "eye-hospital-ee88e.firebasestorage.app",
+  messagingSenderId: "173931536781",
+  appId: "1:173931536781:web:fbf50296e7b05d57235dc4",
+  measurementId: "G-GWM7MZ1FJC"
+};
 
-  };
  
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
@@ -44,4 +44,4 @@ enableIndexedDbPersistence(db)
     });
 
 
-export { db, collection, addDoc, getDocs, query, orderBy, limit, setDoc, doc, where, getDoc, updateDoc, deleteDoc, startAfter, endBefore, limitToLast, auth, signInWithEmailAndPassword};
+export { db, collection, addDoc, getDocs, query, orderBy, limit, setDoc, doc, where, getDoc, updateDoc, deleteDoc, startAfter, endBefore, limitToLast, auth, signInWithEmailAndPassword, runTransaction};
